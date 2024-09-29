39c58f2a40ed6576aed84d806827fd4d5e695f1a
kevinFichou <66632429+kemrode@users.noreply.github.com>
2024-09-29 18:55:21 +0200

    feat(main): réparation du bug d'affichage, à cause de la position Z du rigidbody2d

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,26 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
-    // Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2D;
+    Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position + move * 0.3f * Time.deltaTime;
+        rigidbody2D.MovePosition(position);
     }
 }
